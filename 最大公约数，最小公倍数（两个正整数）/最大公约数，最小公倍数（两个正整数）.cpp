6ff45f9f0bbae3cf7beb8389b32f26fe51da53ee
BrendanTT <zx2508232314@outlook.com>
2023-10-12 20:09:29 +0800

    2nd.最大公约数，最小公倍数

---
+++ 最大公约数，最小公倍数（两个正整数）/最大公约数，最小公倍数（两个正整数）.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	unsigned m, n, max, min, s;
+	int i;
+	s = 0;
+	cout << "Please input integers m and n.(m,n>0):";
+	cin >> m >> n;
+	max = (m >= n) ? m : n;
+	for (i = 1; i <= max; i++)
+	{
+		if ((m % i == 0) && (n % i == 0))
+		{
+			s = i;
+		}
+	}
+	cout << "Greatest common divisor: " << s << endl;//Greatest common divisor.
+	for (i = 1; ; i++)
+	{
+		if (((i * s) % m == 0) && ((i * s) % n == 0))
+		{
+			cout << "Least common multiples: " << (i * s);//Least common multiples.
+			break;
+		}
+	}
+	return 0;
+}
